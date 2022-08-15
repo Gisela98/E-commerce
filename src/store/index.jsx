c0c4e67b663ccb308ccc0ebe2cb6618a44e87167
gisella de los rios <gis1097@hotmail.com>
2022-08-15 12:59:37 -0500

    cambio

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./slices/cart.slice";
+import cartSlice from "./slices/Cart.slice";
 import isLoadingSlice from "./slices/isLoading.slice";
 import productsSlice from "./slices/products.slice";
 import purchaseSlice from "./slices/purchase.slice";
